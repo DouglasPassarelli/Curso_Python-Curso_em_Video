9cc15f029359275c33b8e7294beab5d45d2b8247
DouglasPassarelli <doga.passarelli@hotmail.com>
2022-11-29 01:17:53 -0300

    Adicionando descrição
    Adicionando o problema dos exercicios

--- Mundo-1/ex001.py
+++ Mundo-1/ex001.py
@@ -1,2 +1,4 @@
+#Crie um programa que escreva "Olá, mundo" na tela.
+
 print('\033[34mOlá,Mundo!')
 
--- Mundo-1/ex002.py
+++ Mundo-1/ex002.py
@@ -1,3 +1,5 @@
+#Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+
 nome = input('\033[4;37mQual seu nome ? ')
 print('Muito prazer em te conhecer, {}{}{}!' .format('\033[33m', nome, '\033[m'))
 
